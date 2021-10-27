6dc868fd3493b5e9c75d8e5d6d8b61aa43ce2d65
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-10-26 20:14:03 -0700

    fixed eqArrays and added assertArrayequal

--- eqArrays.js
+++ eqArrays.js
@@ -3,19 +3,7 @@ const assertEqual = function (actual, expected) {
     ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
     : console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
 };
-// this is a version of eqArrays that console.logs the bolean
 
-// const eqArrays = function (array1, array2) {
-//   if (array1.length !== array2.length) {
-//     return console.log(false);
-//   }
-//   for (let i = 0; i < array1.length; i++) {
-//     if (array1[i] === array2[i]) {
-//       return console.log(true);
-//     } else {
-//       return console.log(false);
-//     }
-//   }
 // };
 
 const eqArrays = function (array1, array2) {
@@ -23,12 +11,11 @@ const eqArrays = function (array1, array2) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      return true;
-    } else {
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
+  return true;
 };
 
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
