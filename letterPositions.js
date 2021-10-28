8172a7d8af5f1fd3d4e44a45f0d5049305454397
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-10-27 22:38:59 -0700

    finished letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,7 +1,37 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  }
+  if (!eqArrays(array1, array2)) {
+    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
 const letterPositions = function (sentence) {
-  const str = sentence.split(" ").join("");
+  // const str = sentence.split(" ").join("");
+  // console.log(str);
   const results = {};
-  for (let i = 0; i < str.length; i++) {}
+  for (let i = 0; i < sentence.length; i++) {
+    if (!results[sentence[i]] && sentence[i] !== " ") {
+      results[sentence[i]] = [i];
+    } else if (results[sentence[i]] && sentence[i] !== " ") {
+      results[sentence[i]].push(i);
+    }
+  }
   // logic to update results here
   return results;
 };
+
+assertArraysEqual(letterPositions("hello").e, [1]);
