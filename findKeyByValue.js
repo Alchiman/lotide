87bfcc310eae4cfbdb3503c5950319131c8bbe8b
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-10-28 08:54:06 -0700

    finished findKeyByValue.js using for in loop. Did not use for...of and object.keys! I wonder if that is going to be a problem

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+  return actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+function findKeyByValue(object, value) {
+  for (const property in object) {
+    if (object[property] === value) {
+      return property;
+    }
+  }
+  return undefined;
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
