68f1bc40064a39727638c22c3724f6a56584509c
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-01 15:56:41 -0700

    refactored eqArray to require assertEqual and made eqArrayTest and in it required assertEqual and eqArray

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,4 @@
-const assertEqual = function (actual, expected) {
-  return actual === expected
-    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-};
-
-// };
-
+const assertEqual = require("./assertEqual");
 const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -17,11 +10,4 @@ const eqArrays = function (array1, array2) {
   }
   return true;
 };
-
-// eqArrays([1, 2, 3], [1, 2, 3]); // => true
-// eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
