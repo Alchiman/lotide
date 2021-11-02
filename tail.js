b678866eb0763011f00832f9b04d669227a56ab4
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-01 18:28:46 -0700

    Incorporated Mocha and Chai for middle and tail

--- tail.js
+++ tail.js
@@ -2,11 +2,4 @@ const tail = function (array) {
   return array.slice(1);
 };
 
-const assertEqual = function (actual, expected) {
-  return actual === expected
-    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-};
-
 module.exports = tail;
-
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,19 @@
-const assertEqual = require("../assertEqual");
+// const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
+
+describe("#tail", () => {
+  it("returns [2] for [1, 2]", () => {
+    assert.deepEqual(tail([1, 2]), [2]);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("return [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+});
