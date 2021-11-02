7d61eea6d41087d4f20f998baeb6b800727c2a9b
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-01 17:40:39 -0700

    incorprated Mocha and Chai

--- test/assertArrayEqualTest.js
+++ test/assertArrayEqualTest.js
@@ -1,3 +1,13 @@
 assertArraysEqual = require("../assertArraysEqual");
+
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
 assertArraysEqual([1, 2, 4, 3], [1, 2, 3]); // => false
+// const assert = require("chai").assert;
+// describe("#asserArrayEqual", () => {
+//   it("return [1, 2, 3] ", () => {
+//     assert.strictEqual([1, 2, 3], [1, 2, 3]);
+//   });
+//   it("return ['1', '2', '3']", () => {
+//     assert.strictEqual(["1", "2", "3"], ["1", "2", "3"]);
+//   });
+// });
